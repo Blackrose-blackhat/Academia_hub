fa3ff84b240a381fb8e8f94ea71f1c81069fa50a
musharaf8651 <musharafz2k3@gmail.com>
2023-08-28 00:39:36 +0530

    rules added to firebase database

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -3,17 +3,23 @@ import Cards from '../components/Cards';
 
 import FloatingActionButton from '../components/FloatingActionButton';
 
+
+
 const Home = ({ user }) => {
 
-
+    const isAllowed = user.email.endsWith('@cgu-odisha.ac.in');
     return (
-        <div className=' flex flex-col justify-center align-middle'>
-            {/* <iframe src="https://drive.google.com/file/d/1PzH__T7cPTomg7DG9d-TWZ12EpdTDDbD/preview" height={"500px"} width={"500px"} /> */}
-            <Cards user={user} />
-            <FloatingActionButton />
-
+        <div>
+            {isAllowed ? (<div className=' flex flex-col justify-center align-middle'>
+                {/* <iframe src="https://drive.google.com/file/d/1PzH__T7cPTomg7DG9d-TWZ12EpdTDDbD/preview" height={"500px"} width={"500px"} /> */}
+                <Cards user={user} />
+                <FloatingActionButton />
+            </div>) : (<div className='flex flex-row justify-center align-middle'>
+                <h1 className='text-white text-4xl text-center mt-20'>You do not have access to this web page! please login with your cgu id</h1>
+            </div>)}
 
         </div>
+
     )
 }
 
