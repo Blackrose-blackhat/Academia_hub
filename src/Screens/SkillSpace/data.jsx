81d6ea5f024949b29e00ad68d92eb7e946b67089
Abhiroop <abhiroopkumar1234@gmail.com>
2023-09-03 12:29:00 +0530

    images

--- src/Screens/SkillSpace/data.jsx
+++ src/Screens/SkillSpace/data.jsx
@@ -16,6 +16,19 @@ import codeforces from '../../assets/codeforces.jpg'
 import leetcode from '../../assets/leetcode.jpg'
 import hackerrank from '../../assets/Hackerrank.jpg'
 import hackerearth from '../../assets/hackerearth.jpg'
+import codehelpfree from '../../assets/Codehelpfree.jpg'
+import codehelpaid from '../../assets/codehelppaid.jpg'
+import takeuforward from '../../assets/takeUforward.jpg'
+import Acfree from '../../assets/Apnacollegefree.jpg'
+import Acpaid from '../../assets/ApnaCollegePaid.jpg'
+import dsafcc from '../../assets/DsaFCC.jpg'
+import fccandroid from '../../assets/Androidfcc.jpg'
+import angelaflutter from '../../assets/ANgelayuflutter.png'
+import rivanflutter from '../../assets/flutterRivaan.png'
+import fluttervox from '../../assets/voxmindflutter.png'
+import flutterfcc from '../../assets/Androidfcc.jpg'
+
+
 
 export const learn = [
     {
@@ -171,45 +184,45 @@ export const webdev =[
     {
         id: 1,
         imgsrc: geeksforgeeks,
-        url: "https://www.geeksforgeeks.org/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "GeeksforGeeks"
+        url: "https://www.youtube.com/watch?v=Vi9bxu-M-ag&list=PLDzeHZWIZsTo0wSBcg4-NMIbC0L8evLrD",
+        desc: "Complete Web Development Courseon MERN stack for free on youtube",
+        title: "MERN Stack by Love Babbar"
     },
     {
         id: 2,
         imgsrc: hackerrank,
-        url: "https://www.hackerrank.com/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "HackerRank"
+        url: "https://youtu.be/ZxKM3DCV2kE?si=eyXMCWKWR64et58k",
+        desc: "Full Stack Web Development course on youtube by codedamm ",
+        title: "Full Stack WebDevelopent by codedamm"
     },
     {
         id: 3,
         imgsrc: hackerearth,
-        url:"https://www.hackerearth.com/practice/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "HackerEarth"
+        url:"https://www.youtube.com/@javascriptmastery",
+        desc: "For project based learning, One of the best resources online. Not beginner friendly requires some prerequisites  ",
+        title: "JavascriptMastery "
     },
     {
         id: 4,
         imgsrc: leetcode,
-        url:"https://leetcode.com/problemset/all/",
+        url:"https://www.youtube.com/@WebDevSimplified",
         
-        desc: "Suited for those who are proficient Data Structures & Algorithms",
-        title: "Leetcode"
+        desc: "Another source from where you can learn while making projects in Web Development",
+        title: "WebDEVSimplifies"
     },
     {
         id: 5,
         imgsrc: codechef,
-        url:"https://www.codechef.com/practice",
-        desc: "Suited for experts to practice and brush up their skills in Data Structures & Algorithms",
-        title: "Codechef"
+        url:"https://www.youtube.com/@TraversyMedia",
+        desc: "Learn while you build, TraversyMedia will help you make new projects in Web Development while learning new things ",
+        title: "TraversyMedia"
     },
     {
         id: 6,
         imgsrc: codeforces,
-        url:"https://codeforces.com/problemset",
-        desc: "Suited for experts to practice and brush up their skills in Data Structures & Algorithms",
-        title: "Codeforces"
+        url:"https://www.udemy.com/course/ultimate-web/?gclid=Cj0KCQjwusunBhCYARIsAFBsUP8e3JPhA3kPKr_ugaiePZqLGr2hgvaNZjME23YY67SqbTcXQDmTnncaAmhOEALw_wcB&matchtype=b&utm_campaign=WebDevelopment_v.PROF_la.EN_cc.INDIA_ti.8322_Exp&utm_content=deal4584&utm_medium=udemyads&utm_source=adwords&utm_term=_._ag_82381205698_._ad_670210021613_._kw_angela+yu+web+development_._de_c_._dm__._pl__._ti_kwd-711965347109_._li_9181372_._pd__._",
+        desc: "Learn the fundamentals of Web development from 0 to hero, One of the best courses for beginners ",
+        title: "Web Development Bootcamp-By Angela Yu PAID"
     }
 
 
@@ -218,46 +231,46 @@ export const webdev =[
 export const appdev =[
     {
         id: 1,
-        imgsrc: geeksforgeeks,
-        url: "https://www.geeksforgeeks.org/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "GeeksforGeeks"
+        imgsrc: fccandroid,
+        url: "https://youtu.be/fis26HvvDII?si=6yMbwJfYU13daWrg",
+        desc: "App develpment is one of the most on demand skill right now. Kick-start you Android-dev journey  ",
+        title: "Android App Development by freeCodeCamp"
     },
     {
         id: 2,
-        imgsrc: hackerrank,
-        url: "https://www.hackerrank.com/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "HackerRank"
+        imgsrc: rivanflutter,
+        url: "https://youtu.be/CzRQ9mnmh44?si=q8w4VEMrtQKEQtzO",
+        desc: "Flutter has changed the development journey, Don't miss out on it, Do check out this beginner friendly course on FLutter and Dart  ",
+        title: "Complete Flutter and Dart course by Rivaan Ranawat "
     },
     {
         id: 3,
-        imgsrc: hackerearth,
-        url:"https://www.hackerearth.com/practice/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "HackerEarth"
+        imgsrc: fluttervox,
+        url:"https://youtu.be/My7hgPCapu4?si=x5wR__FtawLQ-FZ2",
+        desc: "FLutter is one of the most on demand skill for android develoment and gaining popularity day by day, Learn the concepts of flutter by VoxMind",
+        title: "FLutter by VOXMind"
     },
     {
         id: 4,
-        imgsrc: leetcode,
-        url:"https://leetcode.com/problemset/all/",
+        imgsrc: flutterfcc,
+        url:"https://youtu.be/VPvVD8t02U8?si=4DOV0BeTSpHEl4Hh",
         
-        desc: "Suited for those who are proficient Data Structures & Algorithms",
-        title: "Leetcode"
+        desc: "Learn flutter development at Freecodecamp from basics to advanced, not for complete beginners",
+        title: "FLutter by Freecodecamp"
     },
     {
         id: 5,
         imgsrc: codechef,
-        url:"https://www.codechef.com/practice",
-        desc: "Suited for experts to practice and brush up their skills in Data Structures & Algorithms",
-        title: "Codechef"
+        url:"https://www.youtube.com/watch?v=cDabx3SjuOY&list=PLQkwcJG4YTCSpJ2NLhDTHhi6XBNfk9WiC",
+        desc: "JetPack compose is comparetively a new technology that is in its early stages of development, Do check out the free course to learn and understand the upcomming technology ",
+        title: "Jetpack Compose"
     },
     {
         id: 6,
-        imgsrc: codeforces,
-        url:"https://codeforces.com/problemset",
-        desc: "Suited for experts to practice and brush up their skills in Data Structures & Algorithms",
-        title: "Codeforces"
+        imgsrc: angelaflutter,
+        url:"https://www.udemy.com/course/flutter-bootcamp-with-dart/?amp=&utm_source=adwords&utm_medium=udemyads&utm_campaign=DSA_Catchall_la.EN_cc.INDIA&utm_content=deal4584&utm_term=_._ag_82569850245_._ad_533220805577_._kw__._de_c_._dm__._pl__._ti_dsa-406594358574_._li_9185433_._pd__._&matchtype=&gclid=EAIaIQobChMI092f7IiMgQMVWNUWBR1srQlcEAAYASAAEgIPHvD_BwE",
+        desc: "One of the best courses for flutter development online. DO check out this course for learning and understaning the fundamentals along with clearing fundamentals",
+        title: "Flutter By Angela Yu Paid"
     }
 
 
@@ -266,46 +279,45 @@ export const appdev =[
 export const DSAlgom =[
     {
         id: 1,
-        imgsrc: geeksforgeeks,
-        url: "https://www.geeksforgeeks.org/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "GeeksforGeeks"
+        imgsrc: codehelpfree,
+        url: "https://www.youtube.com/watch?v=WQoB2z67hvY&list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA",
+        desc: "CodeHelp is a platform where you can learn DSA fro, scratch no prerequisites and required.The course is beginner friendly",
+        title: "CodeHelp By Love Babbar"
     },
     {
         id: 2,
-        imgsrc: hackerrank,
-        url: "https://www.hackerrank.com/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "HackerRank"
+        imgsrc: takeuforward,
+        url: "https://www.youtube.com/watch?v=EAR7De6Goz4&list=PLgUwDviBIf0oF6QL8m22w1hIDC1vJ_BHz",
+        desc: "Take you forward is platform provided by Striver.If you know basics of any language you can check this out.",
+        title: "Take U Forward "
     },
     {
         id: 3,
-        imgsrc: hackerearth,
-        url:"https://www.hackerearth.com/practice/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "HackerEarth"
+        imgsrc: Acfree,
+        url:"https://www.youtube.com/results?search_query=apna+college+dsa+playlist",
+        desc: "Those who have a little knowledge about Data Structures they should definetly check out the placement course by Apna College,this course requires a little understanding about the language and basic syntaxes",
+        title: "Apna College"
     },
     {
         id: 4,
-        imgsrc: leetcode,
-        url:"https://leetcode.com/problemset/all/",
-        
-        desc: "Suited for those who are proficient Data Structures & Algorithms",
-        title: "Leetcode"
+        imgsrc: codehelpaid,
+        url:"https://www.thecodehelp.in/course/data-structures-bootcamp",
+        desc: "Do you want an extra helping hand in your DSA prep, then choose this course.All the necessary resources that are required for DSA are covered here along with assignments",
+        title: "CodeHelp Paid"
     },
     {
         id: 5,
-        imgsrc: codechef,
-        url:"https://www.codechef.com/practice",
-        desc: "Suited for experts to practice and brush up their skills in Data Structures & Algorithms",
-        title: "Codechef"
+        imgsrc: Acpaid,
+        url:"https://www.apnacollege.in/",
+        desc: "Check this course out by Apna college, This covers DSA in JAVA with additional benifits like mentorship sessions and assignments",
+        title: "Apna College  Paid"
     },
     {
         id: 6,
-        imgsrc: codeforces,
-        url:"https://codeforces.com/problemset",
-        desc: "Suited for experts to practice and brush up their skills in Data Structures & Algorithms",
-        title: "Codeforces"
+        imgsrc: dsafcc,
+        url:"https://www.youtube.com/watch?v=8hly31xKli0",
+        desc: "This course is provided by Freecodecamp and is one of the best resources online absolutely free.",
+        title: "DSA by FreecodeCamp"
     }
 
 
@@ -315,46 +327,33 @@ export const Versioncntrl =[
     {
         id: 1,
         imgsrc: geeksforgeeks,
-        url: "https://www.geeksforgeeks.org/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "GeeksforGeeks"
+        url: "https://training.github.com/downloads/github-git-cheat-sheet/",
+        desc: "Cheat Sheet For GIT and GITHUB",
+        title: "Git CheatSheet"
     },
     {
         id: 2,
         imgsrc: hackerrank,
-        url: "https://www.hackerrank.com/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "HackerRank"
+        url: "https://youtu.be/uj4fy4kpaOA?si=i0BLSJpSHakU9H17",
+        desc: "Complete collection of frequently used git and github commands",
+        title: "GIT & GITHUB complete by Love Babbar"
     },
     {
         id: 3,
         imgsrc: hackerearth,
-        url:"https://www.hackerearth.com/practice/",
+        url:"https://youtu.be/apGV9Kg7ics?si=Q9Lai46dxEXX6wdP",
         desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "HackerEarth"
+        title: "GIT and GITHUB Tutorial by Kunal Kushwaha"
     },
     {
         id: 4,
         imgsrc: leetcode,
-        url:"https://leetcode.com/problemset/all/",
+        url:"https://youtu.be/RGOj5yH7evk?si=qNPDVwEO99twuQ2T",
         
-        desc: "Suited for those who are proficient Data Structures & Algorithms",
-        title: "Leetcode"
+        desc: "GIT and GITHUB explained in just one video,not recommended for complete beginners",
+        title: "GIT and GITHUB for beginners by Freecodecamp.org"
     },
-    {
-        id: 5,
-        imgsrc: codechef,
-        url:"https://www.codechef.com/practice",
-        desc: "Suited for experts to practice and brush up their skills in Data Structures & Algorithms",
-        title: "Codechef"
-    },
-    {
-        id: 6,
-        imgsrc: codeforces,
-        url:"https://codeforces.com/problemset",
-        desc: "Suited for experts to practice and brush up their skills in Data Structures & Algorithms",
-        title: "Codeforces"
-    }
+    
 
 
 
@@ -363,45 +362,44 @@ export const CyberSec =[
     {
         id: 1,
         imgsrc: geeksforgeeks,
-        url: "https://www.geeksforgeeks.org/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "GeeksforGeeks"
+        url: "https://www.youtube.com/@NetworkChuck",
+        desc: "If you think cybersecurity is all about hacking you are wrong.The best resource online to start you journey in cybersecurity covering topics from networking to linux and many more  ",
+        title: "NetworkChuck"
     },
     {
         id: 2,
         imgsrc: hackerrank,
-        url: "https://www.hackerrank.com/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "HackerRank"
+        url: "https://www.youtube.com/@davidbombal/videos",
+        desc: "Deep Dive into the sea of cybersecurity with David Bombal ",
+        title: "David Bombal"
     },
     {
         id: 3,
         imgsrc: hackerearth,
-        url:"https://www.hackerearth.com/practice/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "HackerEarth"
+        url:"https://www.coursera.org/learn/foundations-of-cybersecurity",
+        desc: "Build your Computer Security and Networks expertise with this Google Certified course",
+        title: "Foundations of Cybersecurity by Google"
     },
     {
         id: 4,
         imgsrc: leetcode,
-        url:"https://leetcode.com/problemset/all/",
-        
-        desc: "Suited for those who are proficient Data Structures & Algorithms",
-        title: "Leetcode"
+        url:"https://skillsforall.com/",
+        desc: "If you are a networking enthusiast Cisco Networking Academy is the right place for you",
+        title: "Cisco skills for All "
     },
     {
         id: 5,
         imgsrc: codechef,
-        url:"https://www.codechef.com/practice",
-        desc: "Suited for experts to practice and brush up their skills in Data Structures & Algorithms",
-        title: "Codechef"
+        url:"https://portswigger.net/web-security",
+        desc: "Online training platform for cybersecurity enthusiasts ",
+        title: "PortSwigger"
     },
     {
         id: 6,
         imgsrc: codeforces,
-        url:"https://codeforces.com/problemset",
-        desc: "Suited for experts to practice and brush up their skills in Data Structures & Algorithms",
-        title: "Codeforces"
+        url:"https://www.hackthebox.com/",
+        desc: "HackBox is a platform for practising your cybersecurity skills, Recommmended for those who have a sound knowldege in cybersecurirty",
+        title: "HackBox"
     }
 
 
@@ -411,45 +409,44 @@ export const UIndUX =[
     {
         id: 1,
         imgsrc: geeksforgeeks,
-        url: "https://www.geeksforgeeks.org/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "GeeksforGeeks"
+        url: "https://youtu.be/c9Wg6Cb_YlU?si=swT1We-Emb47xTLb",
+        desc: "intrested in UI and UX check out this course by FreeCodeCamp and start your colourful journey in UI and UX ",
+        title: "UI / UX Design Tutorial by FreeCodeCamp"
     },
     {
         id: 2,
         imgsrc: hackerrank,
-        url: "https://www.hackerrank.com/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "HackerRank"
+        url: "https://www.youtube.com/watch?v=ddQHFKVrCkU",
+        desc: "UI-UX Design Full Course for Beginners with Adobe XD and start your journey in UI/Ux",
+        title: "UI-UX Design by WsCube Tech"
     },
     {
         id: 3,
         imgsrc: hackerearth,
-        url:"https://www.hackerearth.com/practice/",
-        desc: "Suited for begineers to practice and brush up their skills in Data Structures & Algorithms",
-        title: "HackerEarth"
+        url:"https://grow.google/intl/en_in/ux-design-course/",
+        desc: "This fully online program provides the skills you need for an entry-level job in UX design, with no prior experience required.",
+        title: "Google UX Design Certificate"
     },
     {
         id: 4,
         imgsrc: leetcode,
-        url:"https://leetcode.com/problemset/all/",
-        
-        desc: "Suited for those who are proficient Data Structures & Algorithms",
-        title: "Leetcode"
+        url:"https://www.youtube.com/watch?v=21FqZhZsi_U&list=PLlHtucAD9KT3wgeeVqILeW-0rYoQgR1Rh&ab_channel=AnshMehra",
+        desc: "Learn the most effective and practical techniques to kickstart your career as a UX designer",
+        title: "Learn UX at $0 by Ansh Mehra"
     },
     {
         id: 5,
         imgsrc: codechef,
-        url:"https://www.codechef.com/practice",
-        desc: "Suited for experts to practice and brush up their skills in Data Structures & Algorithms",
-        title: "Codechef"
+        url:"https://www.youtube.com/@abnux",
+        desc: "Learn UI and UX and design By ABNUX ",
+        title: "ABNUX "
     },
     {
         id: 6,
         imgsrc: codeforces,
-        url:"https://codeforces.com/problemset",
-        desc: "Suited for experts to practice and brush up their skills in Data Structures & Algorithms",
-        title: "Codeforces"
+        url:"https://www.youtube.com/@KingSidharthofficial",
+        desc: "You can explore UI and UX on this channel, One of the best resource for designing Enthusiasts",
+        title: "UI/UX King Sidharth"
     }
 
 
